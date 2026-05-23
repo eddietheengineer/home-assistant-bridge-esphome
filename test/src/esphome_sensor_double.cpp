69d2481132efe630933cf144dcf2d230f2f689cb
eddie <eddietheengr@gmail.com>
2026-05-22 23:47:58 -0400

    more cleanup

--- test/src/esphome_sensor_double.cpp
+++
@@ -1,7 +0,0 @@
-/*!
- * @file
- * @brief ESPHome sensor test double (empty - sensor stub is now a no-op).
- */
-
-/* No implementation needed - sensor stub in test/include/esphome/components/sensor/sensor.h
- * provides inline no-op implementations. */
