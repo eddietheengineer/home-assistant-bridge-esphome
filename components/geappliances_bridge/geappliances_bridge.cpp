9b371b655f21c0b70969afdb7a9d8e134c51b1f2
Eddie The Engineer <eddietheengineer@users.noreply.github.com>
2026-06-14 10:44:38 -0400

    Restore GEA3 request_retries to 10

--- components/geappliances_bridge/geappliances_bridge.cpp
+++ components/geappliances_bridge/geappliances_bridge.cpp
@@ -15,7 +15,7 @@ namespace geappliances_bridge {
 
 static const tiny_gea3_erd_client_configuration_t client_configuration = {
   .request_timeout = 250,
-  .request_retries = 0
+  .request_retries = 10
 };
 
 // GEA2 ERD client: no internal retries.  Bridge-level retries
